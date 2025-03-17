28859ad38c9305fa880ac0d456e64b9725e5d790
Garv <garvmodi269@gmail.com>
2025-03-18 00:23:08 +0530

    Updated Parameters

--- backend/services/email.service.ts
+++ backend/services/email.service.ts
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
 
 export const sendEmails = async (invitations: any[]) => {
   for (const invitation of invitations) {
-    const { email, interviewSessionId, date, time, post } = invitation;
+    const { email, date, time, post } = invitation;
 
     const mailOptions = {
       from: process.env.EMAIL_USER,
@@ -21,10 +21,7 @@ export const sendEmails = async (invitations: any[]) => {
         <p>You have been invited to an interview for the position of <b>${post}</b>.</p>
         <p><b>Date:</b> ${date}</p>
         <p><b>Time:</b> ${time}</p>
-        <p>Click the link below to join your interview:</p>
-        <a href="localhost:5173/candidate/interview/${interviewSessionId}">Join Interview</a>
         <br>
-        <p>You can also use Session ID to join your interview :<b>${interviewSessionId}</b> </p>
         <p>If you have any questions, please contact at ${process.env.EMAIL_USER}.</p>
         <p>Best Regards,<br>Voxhire Team</p>
       `,
