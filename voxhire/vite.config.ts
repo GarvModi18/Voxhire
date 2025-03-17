d09741aab9e5ecbc75af88ea133b78a1a96aacec
Garv <garvmodi269@gmail.com>
2025-03-18 00:21:29 +0530

    Updated Configration

--- voxhire/vite.config.ts
+++ voxhire/vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import tailwindcss from "tailwindcss";
-// import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export default defineConfig({
-  plugins: [
-    react(),
-    //  tailwindcss()
-  ],
-  // css: {
-  //   postcss: "./postcss.config.js",
-  // },
+  plugins: [react()],
+  define: {
+    "process.env": process.env,
+  },
 });
